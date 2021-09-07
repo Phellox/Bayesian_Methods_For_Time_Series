4a57b8fbe150dc4a3d7498885c23209f7091c311
Phellox <35327422+Phellox@users.noreply.github.com>
2021-09-07 13:06:54 +0200

    Initialization of repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementations and comparisons of multiple bayesian methods which can be used to predict time series data. ',
+    author='Bjørn Dahl',
+    license='MIT',
+)
